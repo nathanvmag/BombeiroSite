32160e70496bc4b4fa30be4ea1eb0afc7dd3363b
Nathan Vieira <nathanvmag@gmail.com>
2018-03-25 21:00:28 -0300

    first Commit

---
+++ site/email.php
@@ -0,0 +1,42 @@
+<?php  
+
+if (isset($_POST['name'])&&isset($_POST['email'])){
+
+	$name = $_POST['name'];
+
+	$email= $_POST['email'];	
+
+	$from = $email;
+
+    $to = "rjmimoveismp@gmail.com";
+
+    $subject = "Contato de ".$name;
+
+    $message = "\r\n" ."O email ".$email." quer contactar a empresa";
+
+    $headers = "From:" . $from;
+
+    mail($to,$subject,$message, $headers);
+
+    echo "The email message was sent.";
+
+
+echo "<script>alert('Enviado com Sucesso') </script>";
+
+echo "<script>window.location = 'index.html' </script>";
+
+
+
+
+
+}
+
+else echo "erro";
+
+  ini_set( 'display_errors', 1 );
+
+    error_reporting( E_ALL );
+
+   
+
+?>
